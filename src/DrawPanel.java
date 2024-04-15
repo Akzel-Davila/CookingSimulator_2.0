ecdfbe77aa94773999b756a156fdffeaf4e194f9
BT_4N2_22 <dskjskj@asj>
2024-04-15 13:59:36 -0400

    Refactored the ingredient class to handle a list of ingredients. I also started to implemented a mouse dragging feature.

--- src/DrawPanel.java
+++ src/DrawPanel.java
@@ -1,7 +1,4 @@
-import java.awt.event.KeyEvent;
-import java.awt.event.KeyListener;
-import java.awt.event.MouseListener;
-import java.awt.event.MouseEvent;
+import java.awt.event.*;
 import javax.swing.JPanel;
 import java.awt.Graphics;
 import java.awt.Rectangle;
@@ -9,19 +6,24 @@ import java.awt.Point;
 import java.security.Key;
 import java.util.ArrayList;
 import java.awt.Font;
-class DrawPanel extends JPanel implements MouseListener, KeyListener {
+class DrawPanel extends JPanel implements MouseListener,KeyListener, MouseMotionListener {
     Player p;
+    Ingredients ingredients;
     public DrawPanel() {
         this.addMouseListener(this);
-        setFocusable(true);
         this.addKeyListener(this);
+        this.addMouseMotionListener(this);
+        setFocusable(true);
         p = new Player(1, 50 ,10);
+        ingredients = new Ingredients("lettuce");
     }
 
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
         g.drawRect(p.getX_pos(), p.getY_pos(), p.getImage().getWidth()-100, p.getImage().getHeight()-100);
         g.drawImage(p.getImage(), p.getX_pos(), p.getY_pos(), p.getImage().getWidth()-100, p.getImage().getHeight()-100, null);
+        g.drawRect(50,50,100,100);
+        g.drawImage(ingredients.getImage(0), 50,50,null);
     }
 
     public void mousePressed(MouseEvent e) {
@@ -37,6 +39,20 @@ class DrawPanel extends JPanel implements MouseListener, KeyListener {
     public void mouseEntered(MouseEvent e) { }
     public void mouseExited(MouseEvent e) { }
     public void mouseClicked(MouseEvent e) { }
+    @Override
+    public void mouseDragged(MouseEvent e) {
+        Point clicked = e.getPoint();
+        for (Rectangle hitBox: ingredients.getIngredientBoxes()){
+            if (hitBox.contains(clicked)){
+                g.drawRect(50,50,100,100);
+                g.drawImage(ingredients.getImage(0), 50,50,null);
+            }
+        }
+    }
+
+    @Override
+    public void mouseMoved(MouseEvent e) {
+    }
 
     @Override
     public void keyTyped(KeyEvent e) {
@@ -62,4 +78,5 @@ class DrawPanel extends JPanel implements MouseListener, KeyListener {
     public void keyReleased(KeyEvent e) {
 
     }
+
 }
--- src/Ingredients.java
+++ src/Ingredients.java
@@ -5,26 +5,67 @@ import java.io.File;
 import java.io.IOException;
 
 public class Ingredients {
-    private String imageFileName;
-    private BufferedImage image;
+    private String [] ingredientList;
+    private String [] imageFileNames;
+    private BufferedImage [] images;
+    private int[] cordList;
     private BufferedImage draggedImage;
     private String draggedImageFileName;
     private boolean dragged;
-    private Rectangle ingredientBox;
-    public Ingredients(String ingredientName){
-        this.imageFileName = "images/player_" + ingredientName + ".jpg";
-        this.image = readImage();
-        this.ingredientBox = new Rectangle(-100, -100, image.getWidth(), image.getHeight());
+    private Rectangle[] ingredientBoxes;
+    public Ingredients(String ingredientNames){
+        int startX = 0;
+        int startY = 0;
+        ingredientList = ingredientNames.split(",");
+        cordList = new int[ingredientList.length * 2];
+        images = new BufferedImage[ingredientList.length];
+        imageFileNames = new String[ingredientList.length];
+        ingredientBoxes = new Rectangle[ingredientList.length];
+        for (int h = 0; h < cordList.length; h+=2){
+            cordList[h] = startX;
+            cordList[h+1] = startY;
+            startX += 10;
+            startY += 10;
+        }
+        for (int i = 0; i<ingredientList.length; i++){
+            imageFileNames[i] = "images/ingredient_" + ingredientList[i] + ".jpg";
+            images[i] = readImage(i);
+        }
+        for (int j = 0; j<cordList.length; j+=2){
+            int counter = 0;
+            ingredientBoxes[counter] = new Rectangle(cordList[j], cordList[j+1], images[counter].getWidth(), images[counter].getHeight());
+        }
     }
-    public BufferedImage readImage() {
+
+
+    public String[] getIngredientList() {
+        return ingredientList;
+    }
+
+    public void setIngredientList(String[] ingredientList) {
+        this.ingredientList = ingredientList;
+    }
+    public BufferedImage getImage(int i) {
+        return images[i];
+    }
+
+    public BufferedImage readImage(int i) {
         try {
             BufferedImage image;
-            image = ImageIO.read(new File(imageFileName));
+            image = ImageIO.read(new File(imageFileNames[i]));
             return image;
         }
         catch (IOException e) {
             System.out.println(e);
             return null;
         }
+    }
+
+    public Rectangle[] getIngredientBoxes() {
+        return ingredientBoxes;
+    }
+
+    public void setIngredientBox(Rectangle[] ingredientBoxes) {
+        this.ingredientBoxes = ingredientBoxes;
     }
 }
