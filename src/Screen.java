38089f13f5d01622b523452827c00cd4fc4269ec
BT_4N2_22 <sdkjsdlk#@asj>
2024-05-01 14:06:08 -0400

    began working on screen class

--- src/Screen.java
+++ src/Screen.java
@@ -1,5 +1,10 @@
+import java.awt.*;
+import java.awt.image.BufferedImage;
+
 public class Screen {
     private boolean kitchen;
     private boolean diningRoom;
-    //public Screen()
+    private String [] imageFileName;
+    private BufferedImage[] images;
+    private Rectangle doorHitBox;
 }
